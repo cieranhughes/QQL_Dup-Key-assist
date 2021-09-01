2be2ef653bc3242de867a95c69f63c2642cecb63
cieran <cieranhughes99@googlemail.com>
2021-09-01 23:19:04 +0100

    footer added

--- assets/components/footer.php
+++ assets/components/footer.php
@@ -1,8 +1,28 @@
 <footer>
 	<div class="container-fluid footer-color">
-		<div class="row">
-			<div class="col-sm-12 col-md-6 col-lg-3">
-
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-6 col-lg-3 mt-lg-5 mt-md-5">
+					<h4 class="pb-3" >Find Us</h4>
+					<p>Key Assist<br />3 Axis Court<br />Mallard Way<br />Swansea<br />SA7 0AJ</p>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3 mt-lg-5 mt-md-5">
+					<h4 class="pb-3" >Contact Us</h4>
+					<p>info@key-assist.co.uk<br />03301 072349</p>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3 mt-lg-5 mt-md-5">
+					<h4 class="pb-3" >Useful Links</h4>
+					<p>
+						<a href="">Terms & Conditions</a><br />
+						<a href="">Privacy Policy</a><br />
+						<a href="">Cookies Policy</a>
+					</p>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-3 mt-lg-5 mt-md-5">
+					<img height="60px" width="270px" class="img-fluid" src="/assets/img/logo.png" alt="logo">
+					<p class="pt-1">Key Assist is a trading style of Secpro Limited.</p>
+					<p>Registered office: Axis 3, Mallard Way, Swansea Vale, Swansea, Wales, SA7 0AJ Registered in in England and Wales. Company registration number: 07743548.</p>
+				</div>
 			</div>
 		</div>
 
