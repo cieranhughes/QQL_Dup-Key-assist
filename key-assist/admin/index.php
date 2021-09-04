b661d6a07799bf5a8999426fc0ee85019ebb7206
cieran <cieranhughes99@googlemail.com>
2021-09-04 12:56:28 +0100

    init

---
+++ key-assist/admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Key Assist Admin | <?php echo $page_title; ?></title>
+
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="/assets/css/headerFooter.css">
+	<link rel="stylesheet" href="/assets/css/main.css">
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col text-center">
+				<h1>Admin Page</h1>
+			</div>
+		</div>
+	</div>
+	<!-- Bootstrap, Popper, jQuery -->
+	<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
